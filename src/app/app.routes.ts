cdcd2a5af8c06dfc5ba71784518a81ad9b21015f
coolsurendar <coolsurendar@gmail.com>
2024-04-02 18:48:04 +0200

    Update components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import {Routes} from '@angular/router';
 import {loadRemoteModule} from '@angular-architects/native-federation';
+import {HomeComponent} from './home/home.component';
 
-export const routes: Routes = [{
-  path: '', loadComponent: () =>
-    loadRemoteModule('microfrontend-app-content', './Component').then((m) => m.AppComponent),
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    path: 'home',
+    component:HomeComponent
+  },
+  {
+  path: 'packages', loadComponent: () =>
+    loadRemoteModule('microfrontend-app-content', './PackageListComponent').then((m) => m.PackageListComponent),
 }];
