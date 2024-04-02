a93b0a98cb6570f4dad6809f2fbf19a36d570a85
surendar <surendar.dashinamurthy@littlebigconnection.com>
2024-04-02 15:28:26 +0200

    Init repository

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {loadRemoteModule} from '@angular-architects/native-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: '', loadComponent: () =>
+    loadRemoteModule('microfrontend-app-content', './Component').then((m) => m.AppComponent),
+}];
